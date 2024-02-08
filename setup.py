fb31974fda1d1beab32d2762935c0dce92d59303
amitraofcl <116991792+amitraofcl@users.noreply.github.com>
2024-02-08 03:47:56 -0500

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Telecom Customer Churn Prediction',
+    author='Arijit Mitra',
+    license='',
+)
